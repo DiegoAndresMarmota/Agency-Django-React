e0ea7a2244a0a20e5182fa6338f5336ae51114e9
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-02-11 21:42:36 -0300

    Creación del Layout y {Navbar, Home y Footer}

---
+++ src/components/navigation/Navbar.jsx
@@ -0,0 +1,17 @@
+import { connect } from "react-redux";
+
+function Navbar() {
+    return (
+        <navbar>
+            Navbar
+        </navbar>
+    )
+}
+
+const mapStateToProps = (state) => ({
+
+});
+
+export default connect(mapStateToProps, {
+
+}) (Navbar);
