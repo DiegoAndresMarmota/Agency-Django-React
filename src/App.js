5f579e21191e09cb7e872c00b0852f892dcc9329
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-02-11 21:08:13 -0300

    Implementación Provider store={store}

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import Error404 from "containers/errors/Error404";
 import Home from "containers/pages/Home";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import store from './store';
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Error */}
-        <Route path="*" element={<Error404 />} />
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          {/* Error */}
+          <Route path="*" element={<Error404 />} />
 
-        {/* Inicio*/}
-        <Route path="/" element={<Home />} />
-
-      </Routes>
-    </Router>
+          {/* Inicio*/}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
