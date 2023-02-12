d56c42fed797098a02758ab25f39270711452790
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-02-12 20:27:15 -0300

    Creación de pages del Navbar

---
+++ src/containers/pages/Menu.jsx
@@ -0,0 +1,15 @@
+import Footer from "components/navigation/Footer";
+import Navbar from "components/navigation/Navbar";
+import Layout from "hocs/layouts/Layout";
+
+function Menu() {
+    return (
+        <Layout>
+            <Navbar />
+            <div className="pt-32">Menu</div>
+            <Footer />
+        </Layout>
+    );
+}
+
+export default Menu;
