442af0ac1c391bec7998654084b9ded9ba34bbd1
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-02-12 21:21:56 -0300

    Creación Rutas

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import Error404 from "containers/errors/Error404";
 import Home from "containers/pages/Home";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import store from './store';
+import store from "./store";
 import { Provider } from "react-redux";
+import Recomendaciones from "containers/pages/Recomendaciones";
+import Menu from "containers/pages/Menu";
+import Somos from "containers/pages/Somos";
+import Especialidad from "containers/pages/Especialidad";
+import Blog from "containers/pages/Blog";
+import Contacto from "containers/pages/Contacto";
 
 function App() {
   return (
@@ -15,6 +21,12 @@ function App() {
 
           {/* Inicio*/}
           <Route path="/" element={<Home />} />
+          <Route path="/recomendaciones" element={<Recomendaciones />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/somos" element={<Somos />} />
+          <Route path="/especialidad" element={<Especialidad />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contacto" element={<Contacto />} />
         </Routes>
       </Router>
     </Provider>
