ccef86b47eb8a36f5d69c890172595d422f93b84
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-02-16 00:56:03 -0300

    Uso de OverSubtitle

--- src/containers/pages/Somos.jsx
+++ src/containers/pages/Somos.jsx
@@ -1,3 +1,4 @@
+import OverSubtitle from "components/OverSubtitle";
 import Footer from "components/navigation/Footer";
 import Navbar from "components/navigation/Navbar";
 import SubSomos from "components/somos/SubSomos";
@@ -8,6 +9,7 @@ function Somos() {
         <Layout>
             <Navbar />
             <div className="pt-10">
+                <OverSubtitle />
                 <SubSomos />
             </div>
             <Footer />
