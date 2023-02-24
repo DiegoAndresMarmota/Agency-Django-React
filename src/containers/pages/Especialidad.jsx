ac81f19d27dde92dc5c7b8b66b6dbbff1a4dc1c8
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-02-24 00:39:58 -0300

    Component Especialidad

--- src/containers/pages/Especialidad.jsx
+++ src/containers/pages/Especialidad.jsx
@@ -1,4 +1,5 @@
 import OverSubtitle from "components/OverSubtitle";
+import SpecialityDay from "components/especialidad/SpecialityDay";
 import Footer from "components/navigation/Footer";
 import Navbar from "components/navigation/Navbar";
 import Layout from "hocs/layouts/Layout";
@@ -9,6 +10,7 @@ function Especialidad() {
             <Navbar />
             <div className="pt-10">
                 <OverSubtitle />
+                <SpecialityDay />
             </div>
             <Footer />
         </Layout>
