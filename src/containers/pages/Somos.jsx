d4fbdc8fbb203ce9627e2d773ac9928b24c7cd84
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-02-24 00:46:04 -0300

    component /Somos/Team

--- src/containers/pages/Somos.jsx
+++ src/containers/pages/Somos.jsx
@@ -2,6 +2,7 @@ import OverSubtitle from "components/OverSubtitle";
 import Footer from "components/navigation/Footer";
 import Navbar from "components/navigation/Navbar";
 import SubSomos from "components/somos/SubSomos";
+import Team from "components/somos/Team";
 import Layout from "hocs/layouts/Layout";
 
 function Somos() {
@@ -11,6 +12,7 @@ function Somos() {
             <div className="pt-10">
                 <OverSubtitle />
                 <SubSomos />
+                <Team />
             </div>
             <Footer />
         </Layout>
